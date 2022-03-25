821222c9d06d2e2e715159989db727b8de01200d
AnzeVajnhandl <93084842+AnzeVajnhandl@users.noreply.github.com>
2022-03-25 12:48:24 +0100

    Popravljen WRITE S_ v E

--- kotlin/src/main/kotlin/task/App.kt
+++ kotlin/src/main/kotlin/task/App.kt
@@ -285,7 +285,7 @@ class Recognizer(private val scanner: Scanner) {
     fun recognizeT():Boolean = recognizeX() && recognizeT_()
     fun recognizeE():Boolean = recognizeT() && recognizeE_()
     fun recognizeA():Boolean = recognizeTerminal(VARIABLE) && recognizeTerminal(ASSIGN) && recognizeE()
-    fun recognizeW():Boolean = recognizeTerminal(WRITE) && recognizeS_()
+    fun recognizeW():Boolean = recognizeTerminal(WRITE) && recognizeE()
     fun recognizeFOR():Boolean = recognizeTerminal(FOR) && recognizeA() && recognizeTerminal(TO) && recognizeE() && recognizeTerminal(DO) && recognizeS_() && recognizeTerminal(DONE)
 
     fun recognizeE_():Boolean {
